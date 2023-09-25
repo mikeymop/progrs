61d08848a916d6ff3c298eded563a3d451a64691
mikeymop <michael@defrances.co>
2023-09-24 19:09:20 -0500

    Somethings getting started

---
+++ src/dates.rs
@@ -0,0 +1,6 @@
+pub mod dates {
+
+    pub fn start_of_year() {
+        println!("January, duh!");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
+use clap::{Parser, Subcommand};
+
+use crate::dates::dates::start_of_year;
+
+mod dates;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    // name: Option<String>,
+
+    /// Turn debugging information on
+    /// counts the quantity of the `-d` flag eg: `-ddd`.
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Prints the progress for the current day.
+    Day {
+        /// Override the default utc timezone.
+        #[arg(short, long)]
+        timezone: Option<String>,
+    },
+    /// Prints the progress for the current month.
+    Month {
+        /// Override the default utc timezone.
+        #[arg(short, long)]
+        timezone: Option<String>,
+    },
+    /// Prints the progress for the current year.
+    Year {
+        /// Override the default utc timezone.
+        #[arg(short, long)]
+        timezone: Option<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check the value provided by positional arguments, or option arguments
+    // if let Some(name) = cli.name.as_deref() {
+    //     println!("Value for name: {name}");
+    // }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    println!("Command: {:?}", &cli.command);
+
+    // Match out subcommands.
+    match &cli.command {
+        Some(Commands::Day { timezone }) => {
+            let tz = &timezone.as_ref();
+            println!("Progress for the day in {:?}", tz);
+        }
+        Some(Commands::Month { timezone }) => {
+            let tz = &timezone.as_ref();
+            println!("Progress for the month in {:?}", tz);
+        }
+        Some(Commands::Year { timezone }) => {
+            let tz = &timezone.as_ref();
+            start_of_year();
+            println!("Progress for the year in {:?}", tz);
+        }
+        _ => {
+            println!("Can't decide? Heres the progress for the day.")
+        }
+    }
+
+    // Continued program logic goes here...
 }
