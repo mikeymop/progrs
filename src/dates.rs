2f50bd8b708ff16daede4d2040b9134f9a4e63fe
Michael D <mikeymop@users.noreply.github.com>
2023-10-05 22:35:09 -0500

    Add preliminary support for Workday progress (#3)
    Prelim support for workday

--- src/dates.rs
+++ src/dates.rs
@@ -1,66 +1,160 @@
-#[allow(dead_code)]
-pub mod dates {
-    use std::{time::Duration, thread};
-    use chrono::{Local, Datelike, DateTime, Timelike};
-    use indicatif::ProgressBar;
+use crate::progress::{Progress, OffsetProgress};
+use chrono::{DateTime, Datelike, Local, Timelike};
+use indicatif::ProgressBar;
+use std::{cmp, thread, time::Duration};
 
-    /// Returns a NaiveDate for the start of the supplied month.
-    fn start_of_month(year: i32, month: u32) -> Option<chrono::NaiveDate> {
-        chrono::NaiveDate::from_ymd_opt(year, month, 1)
+/// Returns a NaiveDate for the start of the supplied month.
+fn start_of_month(year: i32, month: u32) -> Option<chrono::NaiveDate> {
+    chrono::NaiveDate::from_ymd_opt(year, month, 1)
+}
+
+/// Returns a NaiveDate for the end of the supplied month.
+fn last_of_month(year: i32, month: u32) -> Option<chrono::NaiveDate> {
+    chrono::NaiveDate::from_ymd_opt(year, month + 1, 1)
+        .or_else(|| chrono::NaiveDate::from_ymd_opt(year + 1, 1, 1))?
+        .pred_opt()
+}
+
+/// Returns the current day of the year
+fn day_of_year(dt: DateTime<Local>) -> u32 {
+    let start = chrono::NaiveDate::from_ymd_opt(dt.year(), 1, 1).unwrap();
+    let dn = dt.date_naive();
+    let diff = dn - start;
+    (diff.num_days() + 1) as u32
+}
+
+/// Generates a progress bar.
+fn mk_progress(min: u32, max: u32, curr: u32, dur: Option<u64>) {
+    let pb = ProgressBar::new(u64::from(max));
+    let end = cmp::min(curr, max);
+
+    for _ in min..end {
+        // print!(" {} ", i);
+        let wait = dur.unwrap_or(50);
+        thread::sleep(Duration::from_millis(wait));
+        pb.inc(1);
     }
 
-    #[allow(dead_code)]
-    /// Returns a NaiveDate for the end of the supplied month.
-    fn last_of_month(year: i32, month: u32) -> Option<chrono::NaiveDate> {
-        chrono::NaiveDate::from_ymd_opt(year, month + 1, 1)
-            .or_else(|| chrono::NaiveDate::from_ymd_opt(year + 1, 1, 1))?
-            .pred_opt()
+    pb.abandon_with_message("Progress for the year");
+}
+
+fn get_percent(curr: u32, max: u32) -> u32 {
+    let percent = f64::from(curr) / f64::from(max) * 100 as f64;
+    println!(
+        "curr: {}, max: {}, percent: {} ({})",
+        curr,
+        max,
+        percent.floor(),
+        percent
+    );
+    cmp::min(percent.floor() as u32, 100 as u32) as u32
+}
+
+pub struct Day {
+    start: u32,
+    end: u32,
+    curr: u32,
+}
+
+impl Progress for Day {
+    fn new() -> Self {
+        let start = 0;
+        let end = 24;
+        let curr = Local::now().hour();
+        Self { start, end, curr }
     }
 
-    fn day_of_year(dt: DateTime<Local>) -> u32 {
-        let start = chrono::NaiveDate::from_ymd_opt(dt.year(), 1, 1).unwrap();
-        let dn = dt.date_naive();
-        let diff = dn - start;
-        (diff.num_days() + 1) as u32
+    fn percent(&self) -> u32 {
+        get_percent(self.curr, self.end)
     }
 
-    fn mk_progress(min: u32, max: u32, curr: u32, dur: Option<u64>) {
-        let pb = ProgressBar::new(u64::from(max));
+    fn print(&self) {
+        mk_progress(self.start, self.end, self.curr, Some(10));
+    }
+}
 
-        for _ in min..curr {
-            let wait = dur.unwrap_or(50);
-            thread::sleep(Duration::from_millis(wait));
-            pb.inc(1);
+pub struct Month {
+    start: u32,
+    end: u32,
+    curr: u32,
+}
+
+impl Progress for Month {
+    fn new() -> Self {
+        let curr = Local::now();
+        let start = start_of_month(curr.year(), curr.month()).unwrap().day0();
+        let end = last_of_month(curr.year(), curr.month()).unwrap().day();
+        Self {
+            start,
+            end,
+            curr: curr.day(),
         }
-
-        pb.abandon_with_message("Progress for the year")
     }
 
-    fn get_percent(curr: u32, max: u32) -> u32 {
-        let percent = f64::from(curr) / f64::from(max) * 100 as f64;
-        // println!("curr: {}, max: {}, percent: {} ({})", curr, max, percent.floor(), percent);
-        percent.floor() as u32
+    fn percent(&self) -> u32 {
+        get_percent(self.curr, self.end)
     }
 
-    pub fn gen_day() -> u32 {
-        let hour = Local::now().hour();
-        mk_progress(0, 24, hour, Some(10));
-        get_percent(hour, 24)
+    fn print(&self) {
+        mk_progress(self.start, self.end, self.curr, Some(10));
+    }
+}
+
+pub struct Year {
+    start: u32,
+    end: u32,
+    curr: u32,
+}
+
+impl Progress for Year {
+    fn new() -> Self {
+        let now = Local::now();
+        let today = day_of_year(now);
+        // let t =
+        Self {
+            start: 0,
+            end: 365,
+            curr: today,
+        }
     }
 
-    pub fn gen_month() -> u32 {
-        let local = Local::now();
-        let start_of_month = start_of_month(local.year(), local.month()).unwrap();
-        let end_of_month = last_of_month(local.year(), local.month()).unwrap();
-        // println!("now: {:?}\nstart of month: day {:?}\nend of month: day {:?}", local, start_of_month.day(), end_of_month.day());
-        mk_progress(start_of_month.day0(), end_of_month.day(), local.day(), Some(10));
-        get_percent(local.day(), end_of_month.day())
+    fn percent(&self) -> u32 {
+        get_percent(self.curr, self.end)
     }
 
-    pub fn gen_year() -> u32 {
-        let local = Local::now();
-        let day = day_of_year(local);
-        mk_progress(0, 365, day, Some(1));
-        get_percent(day, 365)
+    fn print(&self) {
+        mk_progress(self.start, self.end, self.curr, Some(1));
+    }
+}
+
+pub struct Work {
+    start: u32,
+    end: u32,
+    curr: u32,
+}
+
+impl OffsetProgress for Work {
+    fn new(end_of_work: u32) -> Self {
+        // let max = 8;
+        let now = Local::now();
+        let end = chrono::NaiveDate::from_ymd_opt(now.year(), now.month(), now.day())
+            .unwrap()
+            .and_hms_opt(cmp::min(end_of_work, 23), 0, 0)
+            .unwrap();
+
+        Work {
+            start: 0,
+            end: end.hour(),
+            curr: now.hour()
+        }
+    }
+
+    fn percent(&self) -> u32 {
+        get_percent(self.curr, self.end)
+    }
+    
+    fn print(&self) {
+        // mk_progress(0 as u32, max, cmp::min(now.hour(), max), Some(10));
+        mk_progress(self.start, self.end, self.curr, Some(10));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use clap::{Parser, Subcommand};
 
-use crate::dates::dates::{gen_month, gen_year, gen_day};
+use crate::{
+    dates::{Work, Day, Month, Year},
+    progress::{Progress, OffsetProgress}
+};
 
 mod dates;
+mod progress;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    // name: Option<String>,
-
     /// Turn debugging information on
     /// counts the quantity of the `-d` flag eg: `-ddd`.
     #[arg(short, long, action = clap::ArgAction::Count)]
@@ -21,6 +22,14 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Prints the progress for a std 9-5 workday
+    Work {
+        #[arg(short, long)]
+        /// Override the 5pm ending time for the workday.
+        end: Option<String>,
+        /// Override the default timezone.
+        timezone: Option<String>, 
+    },
     /// Prints the progress for the current day.
     Day {
         /// Override the default utc timezone.
@@ -43,15 +52,9 @@ enum Commands {
 
 fn main() {
     let cli = Cli::parse();
-    // println!("Command: {:?}", &cli.command);
 
-    // You can check the value provided by positional arguments, or option arguments
-    // if let Some(name) = cli.name.as_deref() {
-    //     println!("Value for name: {name}");
-    // }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
+    // Debug verbosity level
+    // Note: only flags can have multiple occurrences
     match cli.debug {
         0 => {},
         1 => println!("Debug mode is kind of on"),
@@ -62,34 +65,62 @@ fn main() {
 
     // Match out subcommands.
     match &cli.command {
-        Some(Commands::Day { timezone }) => {
-            let tz = &timezone.as_ref();
-            let percent = gen_day();
+        Some(Commands::Work { end, timezone }) => {
+            let end = end.as_ref();
+            let tz = timezone.as_ref();
+            let end_of_work = end.unwrap_or(&String::from("17")).as_str()
+                .parse::<u32>()
+                .unwrap_or(17);
+            
+            if end.is_some() {
+                println!("Working past 5? 🫠");
+            }
+
             if tz.is_some() {
                 println!("Using tz of {:?}", tz.unwrap());
             }
-            println!("Progress for the day {:?}%", percent);
+            
+            let work = Work::new(end_of_work);
+            println!("Progress for the workday {:?}%", work.percent());
+            work.print();
+        }
+        Some(Commands::Day { timezone }) => {
+            let tz = &timezone.as_ref();
+
+            if tz.is_some() {
+                println!("Using tz of {:?}", tz.unwrap());
+            }
+
+            let day = Day::new();
+            println!("Progress for the day {:?}%", day.percent());
+            day.print();
         }
         Some(Commands::Month { timezone }) => {
             let tz = &timezone.as_ref();
-            let percent = gen_month();
+
             if tz.is_some() {
                 println!("Using tz of {:?}", tz.unwrap());
             }
-            println!("Progress for the month {:?}%", percent);
+
+            let month = Month::new();
+            println!("Progress for the month {:?}%", month.percent());
+            month.print();
         }
         Some(Commands::Year { timezone }) => {
             let tz = &timezone.as_ref();
+
             if tz.is_some() {
                 println!("Using tz of {:?}", tz.unwrap());
             }
-            let percent = gen_year();
-            println!("Progress for the year {:?}%", percent);
+            
+            let year = Year::new();
+            println!("Progress for the year {:?}%", year.percent());
+            year.print();
         }
         _ => {
-            println!("Can't decide? Heres the progress for the day.")
+            println!("Can't decide? Heres the progress for the day.");
+            let day = Day::new();
+            day.print();
         }
     }
-
-    // Continued program logic goes here...
 }
